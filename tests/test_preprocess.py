cde84832d490ef579ee71945a94db4fe77723d2f
matvey <matthew.fedoseev@turintech.ai>
2023-09-20 00:45:33 +0100

    test: pytest fixture for sample data
    test: unit test for `preprocess()`

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,26 +1,45 @@
+import pytest
 import pandas as pd
 
-from src.preprocess import remove_invalid
+from src.preprocess import remove_invalid, preprocess
 
 
-def test_remove_invalid():
-    df = pd.DataFrame(
-        {
-            "StockCode": ["F", "D", "FAILS", "C2", "DOT", "RSF", "M", "M&M", "POST"],
-            "Customer ID": [None, 1, 2, 3, 4, 6, 7, 8, 9],
-            "Invoice": ["F", "1", "C2", "3", "4", "6", "7", "8", "9"],
-        }
-    )
+@pytest.fixture
+def sample_data():
+    data = {
+        "InvoiceDate": ["2009-12-12"] * 5 + ["2010-12-01"] + ["2011-11-11"] * 4,
+        "StockCode": ["UwU", "F", "D", "FAILS", "C2", "DOT", "RSF", "M", "M&M", "POST"],
+        "Customer ID": [0, None, 1, 2, 3, 4, 6, 7, 8, 9],
+        "Invoice": ["55", "F", "1", "C2", "3", "4", "6", "7", "8", "9"],
+    }
+    return pd.DataFrame(data)
+
+
+def test_remove_invalid(sample_data):
+    df = sample_data
 
     result = remove_invalid(df)
 
     expected = pd.DataFrame(
         {
-            "StockCode": ["RSF", "M&M"],
-            "Customer ID": [6.0, 8.0],
-            "Invoice": ["6", "8"],
+            "InvoiceDate": ["2009-12-12", "2010-12-01", "2011-11-11"],
+            "StockCode": ["UwU", "RSF", "M&M"],
+            "Customer ID": [0.0, 6.0, 8.0],
+            "Invoice": ["55", "6", "8"],
         },
-        index=[5, 7],
+        index=[0, 6, 8],
     )
 
     pd.testing.assert_frame_equal(result, expected)
+
+
+def test_preprocess(mocker, sample_data):
+    df_2009_2010 = sample_data.iloc[:6]
+    df_2010_2011 = sample_data.iloc[6:]
+
+    train, test = preprocess(df_2009_2010, df_2010_2011, "2011-10-12")
+
+    assert train.shape[0] == 1
+    assert test.shape[0] == 2
+    assert "C2" not in train["Invoice"].values
+    assert "POST" not in test["StockCode"].values
