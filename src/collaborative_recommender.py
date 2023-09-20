7ca1eca2f480b041097f88fb53e2c05f6404177a
matvey <matthew.fedoseev@turintech.ai>
2023-09-21 00:28:52 +0100

    feat: model saving & loading

--- src/collaborative_recommender.py
+++ src/collaborative_recommender.py
@@ -1,8 +1,9 @@
 from typing import Protocol
+from pathlib import Path
 
 import pandas as pd
-import numpy as np
-from scipy.sparse import coo_matrix, csr_matrix
+from scipy.sparse import coo_matrix
+import joblib
 
 from data_loader import DataLoader
 
@@ -28,6 +29,7 @@ class ImplicitModel(Protocol):
 class CollaborativeRecommender:
     def __init__(self, loader: DataLoader, model: ImplicitModel):
         self.model = model
+        self._fitted = False
 
         df: pd.DataFrame = loader.load_data()
 
@@ -44,19 +46,16 @@ class CollaborativeRecommender:
         self.item_user_coo = coo_matrix((df["Quantity"].astype(float), (df["stock_code"], df["customer_id"])))
         self.user_item_csr = self.item_user_coo.T.tocsr()
 
-        self._fitted = False
-
     def fit(self) -> None:
-        self._fitted = True
         # Calculate the confidence by multiplying it by our alpha value
         alpha_val = 15
         data_conf = (self.item_user_coo * alpha_val).astype("double")
 
         self.model.fit(data_conf)
+        self._fitted = True
 
     def get_recommendations(self, user_id: int) -> pd.DataFrame:
-        if not self._fitted:
-            raise NotFittedError("Model has not been fitted yet.")
+        self._check_if_fitted()
 
         ids, scores = self.model.recommend(user_id, self.user_item_csr[user_id], N=10, filter_already_liked_items=False)
 
@@ -66,3 +65,18 @@ class CollaborativeRecommender:
             items.loc[items.stock_code == str(id_), "score"] = score
 
         return items
+
+    def save_model(self, directory_path: Path) -> None:
+        self._check_if_fitted()
+
+        with open(directory_path / "collaborative_model.joblib", "wb") as f:
+            joblib.dump(self.model, f)
+
+    def load_model(self, model_path: Path) -> None:
+        with open(model_path, "rb") as f:
+            self.model = joblib.load(f)
+            self._fitted = True
+
+    def _check_if_fitted(self) -> None:
+        if not self._fitted:
+            raise NotFittedError("Model has not been fitted yet.")
