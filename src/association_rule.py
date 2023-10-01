108a975f388c0a6edd45e50e2745df215241239f
matvey <matthew.fedoseev@turintech.ai>
2023-10-01 20:59:08 +0100

    test: working association rule mining recommender

--- src/association_rule.py
+++ src/association_rule.py
@@ -3,30 +3,47 @@ from pathlib import Path
 from enum import Enum
 
 from mlxtend.frequent_patterns import apriori, fpgrowth, fpmax, hmine, association_rules
+from mlxtend.preprocessing import TransactionEncoder
 import pandas as pd
 import joblib
+from loguru import logger
 
 from data_loader import DataLoader
 from definitions import NotFittedError
 
 
 class MiningAlgorithm(Enum):
-    APRIORI = apriori
-    FP_GROWTH = fpgrowth
-    FPMAX = fpmax
-    H_MINE = hmine
+    APRIORI = "apriori", apriori
+    FP_GROWTH = "fpgrowth", fpgrowth
+    FPMAX = "fpmax", fpmax
+    H_MINE = "hmine", hmine
+
+    def __new__(cls, label, func):
+        obj = object.__new__(cls)
+        obj._value_ = label
+        obj.func = func
+        return obj
 
     def __call__(self, *args, **kwargs):
-        return self.value(*args, **kwargs)
+        return self.func(*args, **kwargs)
 
 
 class AssociationRuleRecommender:
-    def __init__(self, loader: DataLoader, mining_algorithm: MiningAlgorithm, item_lookup):
-        self.data = loader.load_data()
+    def __init__(self, loader: DataLoader, mining_algorithm: MiningAlgorithm = MiningAlgorithm.APRIORI):
         self.mining_algorithm = mining_algorithm
-        self.item_lookup = item_lookup
+        self.data = loader.load_data()
 
+        self.data = self.data[["StockCode", "Customer ID"]]
+        transactional_encoder = TransactionEncoder()
+        encoded_data = transactional_encoder.fit_transform(
+            self.data.groupby("Customer ID")["StockCode"].apply(list).values
+        )
+        self.data = pd.DataFrame(encoded_data, columns=transactional_encoder.columns_)
+
+        self.rules = None
         self._fitted = False
+
+        logger.info("AssociationRuleRecommender initialised")
 
     def fit(self):
         frequent_itemsets = self._frequent_itemset_mining()
@@ -34,15 +51,21 @@ class AssociationRuleRecommender:
         self.rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
         self._fitted = True
 
-    def get_recommendations(self):
+    def get_recommendations(self, item_id: str) -> pd.DataFrame:
         self._check_if_fitted()
-        pass
 
-    def _frequent_itemset_mining(self, min_support: float = 0.1) -> pd.DataFrame:
+        recommendations = self.rules[self.rules["antecedents"].apply(lambda x: item_id in x)]
+
+        recommendations = recommendations.sort_values(by="lift", ascending=False)
+        recommendations = recommendations[["antecedents", "consequents", "lift"]]
+
+        return recommendations
+
+    def _frequent_itemset_mining(self, min_support: float = 0.01) -> pd.DataFrame:
         """ Perform frequent itemset mining using the specified algorithm and minimum support.
 
         Args:
-            min_support: The minimum support of the itemsets to be returned. Defaults to 0.1.
+            min_support: The minimum support of the itemsets to be returned. Defaults to 0.01.
 
         Returns:
             A DataFrame of frequent itemsets.
@@ -50,7 +73,9 @@ class AssociationRuleRecommender:
         Raises:
             ValueError: If an invalid mining algorithm is specified.
         """
+
         if self.mining_algorithm not in MiningAlgorithm:
+            logger.error(f"Invalid mining algorithm : {self.mining_algorithm}")
             raise ValueError(f"Invalid mining algorithm : {self.mining_algorithm}")
 
         return self.mining_algorithm(self.data, min_support=min_support, use_colnames=True)
@@ -66,6 +91,32 @@ class AssociationRuleRecommender:
         with open(model_path, "rb") as f:
             return joblib.load(f)
 
+    def cache_rules(self, directory_path: Path) -> None:
+        """Caches the association rules."""
+
+        self._check_if_fitted()
+
+        try:
+            with open(directory_path / "association_rules.csv", "w") as f:
+                self.rules.to_csv(f, index=False)
+        except Exception as e:
+            logger.error(f"An error occurred while caching the rules: {e}")
+            raise
+
+    def load_rules_from_cache(self, rule_path: Path) -> None:
+        """Loads the association rules from cache."""
+
+        try:
+            self.rules = pd.read_csv(rule_path)
+            self._fitted = True
+            logger.info("Rules loaded successfully")
+        except Exception as e:
+            logger.error(f"An error occurred while loading the rules: {e}")
+            raise
+
     def _check_if_fitted(self) -> None:
         if not self._fitted:
             raise NotFittedError("Model has not been fitted yet.")
+
+        elif self.rules is None:
+            raise ValueError("No rules found. Please fit the model first to generate rules.")
